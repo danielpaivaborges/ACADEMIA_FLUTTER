d53b7982fb958c5b2dfcede5692511bd27ccccc7
DANIEL PAIVA BORGES <danielpaivaborges@outlook.com>
2023-05-23 15:51:59 -0300

    atualização

--- DART/inicio/lib/funcoes_colecoes/exceptions.dart
+++ DART/inicio/lib/funcoes_colecoes/exceptions.dart
@@ -1,1 +1,14 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  var idade = "50 anos";
+  String? nome;
+
+  try {
+    var convertido = int.parse(idade);
+  } on FormatException catch (s) {
+    print("ocorreu um erro de formatação $s");
+  } catch (e, s) {
+    print("ocorreu outro erro $s");
+  } finally {
+    print("executou");
+  }
+}
