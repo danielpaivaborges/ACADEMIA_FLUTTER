a76fb01f7462e0d16c729a39b100a27f0e2fa801
DANIEL PAIVA BORGES <danielpaivaborges@outlook.com>
2023-05-25 21:22:50 -0300

    atualizando

--- DART/colecoes/lib/desafio/main.dart
+++ DART/colecoes/lib/desafio/main.dart
@@ -1,5 +1,4 @@
 void main() {
-  //! Detalhe sobre a String
   //! A String é composta por 3 campos (Nome|Idade|Sexo)
   final pessoas = [
     'Rodrigo Rahman|35|Masculino',
@@ -21,10 +20,64 @@ void main() {
 
   //! Baseado na lista acima.
   //! 1 - Remova os pacientes duplicados e apresente a nova lista
+  print('*****removendo duplicados*****');
+  var lista = pessoas.toSet().map((e) => e.split("|")).toList();
+  for (var pessoa in lista) {
+    print(pessoa[0]);
+  }
 
   //! 2 - Me mostre a quantidade de pessoas por sexo (Masculino e Feminino) e depois me apresente o nome delas
 
+  print(
+      'Me mostre a quantidade de pessoas por sexo (Masculino e Feminino) e depois me apresente o nome delas');
+  var mapaSexo = <String, List<String>>{};
+
+  for (var pessoa in lista) {
+    final masculinos = mapaSexo["M"] ?? <String>[];
+    final femininos = mapaSexo["F"] ?? <String>[];
+    var sexo = pessoa[2].toLowerCase();
+
+    if (sexo == "masculino") {
+      masculinos.add(pessoa[0]);
+    } else {
+      femininos.add(pessoa[0]);
+    }
+
+    mapaSexo["M"] = masculinos;
+    mapaSexo["F"] = femininos;
+  }
+
+  var masculino = mapaSexo["M"] ?? <String>[];
+  var feminino = mapaSexo["F"] ?? <String>[];
+
+  print("o numero de indivíduos do sexo masculino é ${masculino.length}");
+  for (var nome in masculino) {
+    print(nome);
+  }
+  print("o numero de individuos do sexo feminino é ${feminino.length}");
+  for (var nome in feminino) {
+    print(nome);
+  }
+
   //! 3 - Filtrar e deixar a lista somente com pessoas maiores de 18 anos e apresente essas pessoas pelo nome
+  var maiores = lista.where((element) {
+    return (int.tryParse(element[1]) ?? 0) > 18;
+  });
+  print(
+      '3 - Filtrar e deixar a lista somente com pessoas maiores de 18 anos e apresente essas pessoas pelo nome');
+  for (var pessoa in maiores) {
+    print(pessoa[0]);
+  }
 
   //! 4 - Encontre a pessoa mais velha e apresente o nome dela.
+  print('4 - Encontre a pessoa mais velha e apresente o nome dela.');
+  var listaNova = [...lista];
+
+  listaNova.sort((p1, p2) {
+    var idade1 = (int.tryParse(p1[1])) ?? 0;
+    var idade2 = (int.tryParse(p2[1])) ?? 0;
+    return idade1.compareTo(idade2);
+  });
+  print(
+      'A pessoa mais velha da lista é ${listaNova.last[0]} e tem a idade de ${listaNova.last[1]} anos');
 }
