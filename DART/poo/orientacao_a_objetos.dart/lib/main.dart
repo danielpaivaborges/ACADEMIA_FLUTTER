1cfa5a272faad05b1946cab7907cb5b96ff1347c
DANIEL PAIVA BORGES <danielpaivaborges@outlook.com>
2023-05-29 19:24:21 -0300

    hoje

--- DART/poo/orientacao_a_objetos.dart/lib/main.dart
+++ DART/poo/orientacao_a_objetos.dart/lib/main.dart
@@ -1,1 +1,12 @@
-void main(List<String> args) {}
+import 'package:orientacao_a_objetos/classes/camisesta.dart';
+
+void main(List<String> args) {
+  var camiseta1 = Camiseta(cor: 'preta', marca: 'Nike', tamanho: "P");
+
+  print(camiseta1.toString());
+
+  var camiseta2 = Camiseta(cor: 'branca', marca: 'Addidas', tamanho: 'G');
+  print(camiseta2.toString());
+
+  print(Camiseta.nomeIndustrial);
+}
