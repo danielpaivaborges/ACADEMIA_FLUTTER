86887b50f12b17a6c7f500d4838239cdceaabac5
DANIEL PAIVA BORGES <danielpaivaborges@outlook.com>
2023-05-29 19:55:54 -0300

    atualizado

--- DART/poo/orientacao_a_objetos.dart/lib/classes/camisesta.dart
+++ DART/poo/orientacao_a_objetos.dart/lib/classes/camisesta.dart
@@ -1,13 +1,18 @@
 class Camiseta {
   String tamanho = "G";
-  String cor = 'preta';
+  String? _cor;
   String? marca;
+
+  String? get cor => _cor;
+  set corExterna(String? cor) {
+    _cor = cor ?? "azul";
+  }
 
   static int contador = 0;
   //crie com const para não poder ser alterado
   static const nomeIndustrial = "Camiseta indstrial";
 
-  Camiseta({required this.cor, required this.marca, required this.tamanho}) {
+  Camiseta({required this.marca, required this.tamanho}) {
     contador++;
     print(
         "uma instancia de camiseta acaba de ser criada...Agora temos $contador camiseta(s) criada(s)");
@@ -16,6 +21,6 @@ class Camiseta {
   @override
   String toString() {
     super.toString();
-    return "A camiseta da marca $marca,  de tamanho $tamanho e de cor $cor acaba de ser criada.";
+    return "A camiseta da marca $marca,  de tamanho $tamanho e de cor ${cor ?? "azul"} acaba de ser criada.";
   }
 }
--- DART/poo/orientacao_a_objetos.dart/lib/main.dart
+++ DART/poo/orientacao_a_objetos.dart/lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:orientacao_a_objetos/classes/camisesta.dart';
 
 void main(List<String> args) {
-  var camiseta1 = Camiseta(cor: 'preta', marca: 'Nike', tamanho: "P");
+  var camiseta1 = Camiseta(marca: 'Nike', tamanho: "P");
+  camiseta1.corExterna = "verde";
 
   print(camiseta1.toString());
 
-  var camiseta2 = Camiseta(cor: 'branca', marca: 'Addidas', tamanho: 'G');
+  var camiseta2 = Camiseta(marca: 'Addidas', tamanho: 'G');
+
   print(camiseta2.toString());
 
   print(Camiseta.nomeIndustrial);
