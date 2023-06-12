f8c928c994a6ae83d4c61f80c94dba7f57920e80
DANIEL PAIVA BORGES <danielpaivaborges@outlook.com>
2023-06-12 08:18:51 -0300

    alterando2

---
+++ DART/assincrono/bin/assincrono.dart
@@ -0,0 +1,5 @@
+import 'package:assincrono/assincrono.dart' as assincrono;
+
+void main(List<String> arguments) {
+  print('Hello world: ${assincrono.calculate()}!');
+}
---
+++ DART/assincrono/test/assincrono_test.dart
@@ -0,0 +1,8 @@
+import 'package:assincrono/assincrono.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
