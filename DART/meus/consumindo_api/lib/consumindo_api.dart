8e1a2a33b84e8341237311aa9548314a353d0a66
DANIEL PAIVA BORGES <danielpaivaborges@outlook.com>
2023-06-20 08:14:52 -0300

    limpando

--- DART/meus/consumindo_api/lib/consumindo_api.dart
+++
@@ -1,7 +0,0 @@
-import 'package:consumindo_api/repository/cliente_repository.dart';
-
-Future<void> main(List<String> args) async {
-  final clientes = ClienteRepository();
-  final resposta = await clientes.recuperaClientes();
-  print(resposta);
-}
